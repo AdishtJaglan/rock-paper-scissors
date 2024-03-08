be5aab6e2ed93af4e7957ece0f73c826ddf6a281
AdishtJaglan <adisht7524@gmail.com>
2024-03-08 20:59:27 +0530

    add final files

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-console.log("Testing!");
+const getComputerChoice = () => {
+    const choice = ["rock", "paper", "scissors"];
+
+    return choice[Math.floor(Math.random() * 3)];
+};
+
+let playerScore = 0;
+let computerScore = 0;
+
+const singleRoundOfGame = (playerSelection, computerSelection) => {
+    playerSelection = playerSelection.toLowerCase();
+
+    if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
+        playerScore++;
+        return `Player wins! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        computerScore++;
+        return `Computer wins! ${computerSelection} beats ${playerSelection}`;
+    }
+};
+
+const playGame = () => {
+    for (let i = 0; i < 5; i++) {
+        playerChoice = window.prompt("Rock, paper or scissors?");
+        console.log(singleRoundOfGame(playerChoice, getComputerChoice()));
+    }
+
+    if (playerScore > computerScore) {
+        console.log("Player wins!");
+    } else {
+        console.log("Computer wins!");
+    }
+};
+
+playGame();
